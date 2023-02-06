3d1b610f6bff1fe1eb37d1877dd667498e79f35a
vxy2715 <vxy2715@gmail.com>
2023-02-07 00:30:24 +0900

    githubAction

---
+++ src/main/java/com/codestates/practicegithebActiondeploy/PracticeGithebActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithebActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithebActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithebActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithebActiondeploy/PracticeGithebActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithebActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithebActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
